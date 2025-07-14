ee590e515e125dbec1282aa84e312cc55dadeb29
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-14 13:17:46 -0400

    Basic game logic

--- Guess My Number!/script.js
+++ Guess My Number!/script.js
@@ -1,1 +1,17 @@
 "use scrict";
+const target = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
+    if (!guess) {
+        document.querySelector(".message").textContent = "🚫 No number!";
+    } else if (guess === target) {
+        document.querySelector(".message").textContent = "🎉 Correct Number!";
+        check = false;
+    } else if (guess >= target) {
+        document.querySelector(".message").textContent = "📈 Too high!";
+    } else if (guess <= target) {
+        document.querySelector(".message").textContent = "📉 Too low!";
+    } else {
+    }
+});

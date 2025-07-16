9fd0532355fc92338524b0244cc9ac08bdb79f5d
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-16 16:52:53 -0400

    bog fix

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,7 +1,10 @@
 "use strict";
 
 // Selecting elements
-const playersEl = [document.querySelector(".player1"), document.querySelector(".player2")];
+const playersEl = [
+    document.querySelector(".player1"),
+    document.querySelector(".player2"),
+];
 const currentScoresEl = document.querySelectorAll(".current-score");
 const totalScoresEl = document.querySelectorAll(".total-score");
 const btnNewGameEl = document.querySelector(".btn-restart");
@@ -9,11 +12,24 @@ const btnRollEl = document.querySelector(".btn-roll");
 const btnHoldEl = document.querySelector(".btn-hold");
 const diceEl = document.querySelector(".dice");
 
-// Starting conditionsbtn
-let currentScore = 0
-let scores = [0, 0];
-let currentPlayer = 0;
-diceEl.classList.add("hidden");
+// Starting conditions
+let scores, currentScore, currentPlayer, playing;
+function initialValue() {
+    scores = [0, 0];
+    currentScore = 0;
+    currentPlayer = 0;
+    playing = true;
+    for (let i = 0; i < 2; i++) {
+        currentScoresEl[i].textContent = 0;
+        totalScoresEl[i].textContent = 0;
+        playersEl[i].classList.remove("player-winner");
+        playersEl[i].classList.remove("player-active");
+    }
+
+    playersEl[0].classList.add("player-active");
+    diceEl.classList.add("hidden");
+}
+initialValue();
 
 // switch to the other player
 function swapPlayer() {
@@ -26,41 +42,35 @@ function swapPlayer() {
 
 // rolling dice
 btnRollEl.addEventListener("click", function () {
-    const diceRoll = Math.trunc(Math.random() * 6) + 1;
-    // display dice
-    diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${diceRoll}.png`;
-    if (diceRoll === 1) {
-        swapPlayer();
-    } else {
-        currentScore += diceRoll;
-        currentScoresEl[currentPlayer].textContent = currentScore;
+    if (playing) {
+        const diceRoll = Math.trunc(Math.random() * 6) + 1;
+        // display dice
+        diceEl.classList.remove("hidden");
+        diceEl.src = `dice-${diceRoll}.png`;
+        if (diceRoll === 1) {
+            swapPlayer();
+        } else {
+            currentScore += diceRoll;
+            currentScoresEl[currentPlayer].textContent = currentScore;
+        }
     }
 });
 
 // hold the score
 btnHoldEl.addEventListener("click", function () {
-    scores[currentPlayer] += currentScore;
-    totalScoresEl[currentPlayer].textContent = scores[currentPlayer];
-    if (scores[currentPlayer] >= 100) {
-        playersEl[currentPlayer].classList.add("player-winner");
-        // lock the buttons
-    } else {
-        swapPlayer();
+    if (playing) {
+        scores[currentPlayer] += currentScore;
+        totalScoresEl[currentPlayer].textContent = scores[currentPlayer];
+        if (scores[currentPlayer] >= 100) {
+            playing = false;
+            playersEl[currentPlayer].classList.add("player-winner");
+            diceEl.classList.add("hidden");
+            // lock the buttons
+        } else {
+            swapPlayer();
+        }
     }
 });
 
 // reset the game
-btnNewGameEl.addEventListener("click", function () {
-    currentScore = 0
-    scores = [0, 0];
-    currentPlayer = 0;
-    for (let i = 0; i < 2; i++) {
-        currentScoresEl[i].textContent = 0;
-        totalScoresEl[i].textContent = 0;
-        playersEl[i].classList.remove("player-winner");
-        playersEl[i].classList.remove("player-active");
-    }
-    playersEl[0].classList.add("player-active");
-    diceEl.classList.add("hidden");
-});
+btnNewGameEl.addEventListener("click", initialValue);

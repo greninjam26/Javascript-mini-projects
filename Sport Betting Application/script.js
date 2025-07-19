04d46c4174e39b30b44b8a99efc2550a69de6a1b
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-19 08:36:32 -0400

    fixes

--- Sport Betting Application/script.js
+++ Sport Betting Application/script.js
@@ -59,18 +59,25 @@ const allPlayer = [...players1, ...players2];
 // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-const { team1, draw, team2 } = game.odds;
+const { team1, x: draw, team2 } = game.odds;
 // 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 function printGoals(...players) {
-    for (let j = 0; j < players.length; i++) {
-        let scores = 0;
-        for (let i = 0; i < game.scored.length; i++) {
-            if (game.scored[i] === players[j]) {
-                scores++;
-            }
-        }
-        console.log(`${players[j]} scored ${scores}`);
+    // for (let j = 0; j < players.length; j++) {
+    //     let scores = 0;
+    //     for (let i = 0; i < game.scored.length; i++) {
+    //         if (game.scored[i] === players[j]) {
+    //             scores++;
+    //         }
+    //     }
+    //     console.log(`${players[j]} scored ${scores}`);
+    // }
+    for (let i = 0; i < players.length; i++) {
+        console.log(players[i]);
     }
+    console.log(`${players.length} goals were scored`);
 }
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals(...game.scored);
 // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
-console.log();
+team1 < team2 && console.log("Team 1 is more likely to win");
+team2 < team1 && console.log("Team 2 is more likely to win");

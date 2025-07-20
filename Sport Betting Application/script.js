452bbbf77e9de93e6e4da5a4b3bf3cb12cead4fa
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-20 14:47:23 -0400

    improvment

--- Sport Betting Application/script.js
+++ Sport Betting Application/script.js
@@ -134,15 +134,16 @@ const gameEvents = new Map([
     [92, "🔶 Yellow card"],
 ]);
 // 1. Create an array 'events' of the different game events that happened (no duplicates)
-const events = [...new Set([...gameEvents.values()])];
+const events = [...new Set(gameEvents.values())];
 console.log(events);
 // 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
 gameEvents.delete(64);
 console.log(gameEvents);
 // 3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+const time = [...gameEvents.keys()].pop();
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
 // 4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
 //     [FIRST HALF] 17: ⚽ GOAL
 for (const [time, event] of gameEvents) {
-    console.log(`[${time < 45 ? "FIRST" : "SECOND"} HALF] ${time}: ${event}`);
+    console.log(`[${time <= 45 ? "FIRST" : "SECOND"} HALF] ${time}: ${event}`);
 }

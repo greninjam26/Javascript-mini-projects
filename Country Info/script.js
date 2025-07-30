d7e6db809f7ad09726f06dc2a41bdd1eecfcdd9a
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-07-30 17:20:12 -0400

    add country card

--- Country Info/script.js
+++ Country Info/script.js
@@ -1,12 +1,28 @@
 "use strict";
-        // <article class="country">
-        //   <img class="country__img" src="" />
-        //   <div class="country__data">
-        //     <h3 class="country__name">COUNTRY</h3>
-        //     <h4 class="country__region">REGION</h4>
-        //     <p class="country__row"><span>👫</span>POP people</p>
-        //     <p class="country__row"><span>🗣️</span>LANG</p>
-        //     <p class="country__row"><span>💰</span>CUR</p>
-        //   </div>
-        // </article>
 
+// DOM selection
+const countriesContainer = document.querySelector('.countries');
+
+// old way
+const request = new XMLHttpRequest();
+// type of request, the API link
+request.open("GET", "https://countries-api-836d.onrender.com/countries/name/Canada");
+request.send();
+request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `        
+        <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>
+    `;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+});

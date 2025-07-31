6823819d30c3d744b5acce4cf320272830ad2065
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-07-30 20:44:27 -0400

    add display for neighbouring countries

--- Country Info/script.js
+++ Country Info/script.js
@@ -3,20 +3,14 @@
 // DOM selection
 const countriesContainer = document.querySelector(".countries");
 
-// old way
-const createCountryCardOld = function (country) {
-    const request = new XMLHttpRequest();
-    // type of request, the API link
-    request.open("GET", `https://countries-api-836d.onrender.com/countries/name/${country}`);
-    request.send();
-    request.addEventListener("load", function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
+// utility variables
+const countries = [];
 
-        const html = `        
-        <article class="country">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
+const createCountryCard = function (data, className = "") {
+    const html = `        
+        <article class="country ${className}">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
             <h4 class="country__region">${data.region}</h4>
             <p class="country__row"><span>👫</span>${Math.round(
@@ -24,16 +18,54 @@ const createCountryCardOld = function (country) {
             ).toFixed(1)} million people</p>
             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-          </div>
+        </div>
         </article>
     `;
-        countriesContainer.insertAdjacentHTML("beforeend", html);
-        countriesContainer.style.opacity = 1;
-    });
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country, stats, className = false) {
+    countries.push(country);
+    // OLD WAY
+    // const request = new XMLHttpRequest();
+    // // type of request, the API link
+    // request.open(
+    //     "GET",
+    //     `https://countries-api-836d.onrender.com/countries/${stats}/${country}`
+    // );
+    // request.send();
+    // request.addEventListener("load", function () {
+    //     const data =
+    //         stats === "alpha"
+    //             ? JSON.parse(this.responseText)
+    //             : JSON.parse(this.responseText)[0];
+    //     console.log(data);
+    //     createCountryCard(data, className);
+    //     // get neighbouring countries, this can make sure the neighbours will be executed after the main one
+    //     if (!className) {
+    //         data.borders.forEach(coun => {
+    //             if (!countries.includes(coun)) {
+    //                 getCountryData(coun, "alpha", "neighbour");
+    //             }
+    //         });
+    //     }
+    // });
+
+    // NEW WAY(promises(ADDED in ES6))
+    //  this return an object is a promise(this is a placeholder for results of an asynchronous code)
+    // with promises we don't need events and callbacks anymore
+    // we can chain promises
+    const request = fetch(`https://countries-api-836d.onrender.com/countries/${stats}/${country}`);
+    
 };
 
 // everytime it is in different order because the data comes in at different time
-createCountryCardOld("USA");
-createCountryCardOld("canada");
-createCountryCardOld("china");
-createCountryCardOld("japan");
+// to make
+// getCountryData("CAN", "alpha");
+getCountryData("USA", "alpha");
+// getCountryData("japan", "name");
+// getCountryData("united kingdom", "name");
+
+// too many neighbours
+// getCountryData("china", "name");
